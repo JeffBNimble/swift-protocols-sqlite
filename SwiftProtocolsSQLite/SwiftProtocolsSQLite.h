cfd61f0e312ea263f05cdf96bfdd462cdfc2b98c
Jeff Roberts <jeff@nimbleNogginSoftware.com>
2015-07-12 18:42:03 -0500

    Initial Commit

---
+++ SwiftProtocolsSQLite/SwiftProtocolsSQLite.h
@@ -0,0 +1,19 @@
+//
+//  SwiftProtocolsSQLite.h
+//  SwiftProtocolsSQLite
+//
+//  Created by Jeffrey Roberts on 7/12/15.
+//  Copyright © 2015 NimbleNoggin.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftProtocolsSQLite.
+FOUNDATION_EXPORT double SwiftProtocolsSQLiteVersionNumber;
+
+//! Project version string for SwiftProtocolsSQLite.
+FOUNDATION_EXPORT const unsigned char SwiftProtocolsSQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftProtocolsSQLite/PublicHeader.h>
+
+
