2ccb0cd3b82a854a297eee6025084cb2854a68eb
Jeff Roberts <jeff@nimbleNogginSoftware.com>
2015-07-13 05:30:17 -0500

    Adding basic protocols

---
+++ database/Cursor.swift
@@ -0,0 +1,162 @@
+//
+// Created by Jeffrey Roberts on 7/12/15.
+// Copyright (c) 2015 NimbleNoggin.io. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Cursor represents a collection of zero or more rows. Each row contains 1 or more
+ * columns identified either by index or name. Each column contains data of a specific
+ * type. The cursor can be positioned forward, backward, relatively, absolutely and to the
+ * first and last rows. When finished with a Cursor, it should be closed.
+ */
+protocol Cursor {
+    /// boolFor: Answer the boolean value at the specified column name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The Bool value at the requested column
+    func boolFor(columnName:String) -> Bool
+    
+    /// boolFor: Answer the boolean value at the specified column name
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The Bool value at the requested column
+    func boolFor(columnIndex:Int) -> Bool
+    
+    /// close: Close the cursor
+    /// Returns: void
+    func close() -> Void
+    
+    /// columnCount: The number of columns in the cursor
+    /// Returns: The number of columns in this cursor
+    func columnCount() -> Int
+    
+    /// columnIndexFor: Answer the index of the specified column name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The index at the requested column or -1 if no such column
+    func columnIndexFor(columnName:String) -> Int
+    
+    /// columnAtIndexIsNull: Answer whether the specified column has a null value
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: A Bool indicating whether the column value is null
+    func columnAtIndexIsNull(columnIndex:Int) -> Bool
+    
+    /// columnIsNull: Answer whether the specified column has a null value
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: A Bool indicating whether the column value is null
+    func columnIsNull(columnName:String) -> Bool
+    
+    /// columnNameFor: Answer the name of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The name of the requested column
+    func columnNameFor(columnIndex:Int) -> String
+    
+    /// dataFor: Answer the NSData value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The NSData value of the requested column
+    func dataFor(columnName:String) -> NSData
+    
+    /// dataFor: Answer the NSData value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The NSData value of the requested column
+    func dataFor(columnIndex:Int) -> NSData
+    
+    /// dateFor: Answer the NSDate value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The NSDate value of the requested column
+    func dateFor(columnName:String) -> NSDate
+    
+    /// dateFor: Answer the NSDate value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The NSDate value of the requested column
+    func dateFor(columnIndex:Int) -> NSDate
+    
+    /// doubleFor: Answer the Double value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The Double value of the requested column
+    func doubleFor(columnName:String) -> Double
+    
+    /// doubleFor: Answer the Double value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The Double value of the requested column
+    func doubleFor(columnIndex:Int) -> Double
+    
+    /// intFor: Answer the Int value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The Int value of the requested column
+    func intFor(columnName:String) -> Int
+    
+    /// intFor: Answer the Int value of the column at the specified index
+    /// Parameter columnIndex: The indix of the column being referenced
+    /// Returns: The Int value of the requested column
+    func intFor(columnIndex:Int) -> Int
+    
+    /// longFor: Answer the Int32 value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The Int32 value of the requested column
+    func longFor(columnName:String) -> Int32
+    
+    /// longFor: Answer the Int32 value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The Int32 value of the requested column
+    func longFor(columnIndex:Int) -> Int32
+    
+    /// longLongFor: Answer the Int64 value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The Int64 value of the requested column
+    func longLongIntFor(columnName:String) -> Int64
+    
+    /// longLongFor: Answer the Int64 value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The Int64 value of the requested column
+    func longLongIntFor(columnIndex:Int) -> Int64
+    
+    /// moveTo: Move the row pointer forward or backward a specified number of rows
+    /// Parameter offset: The number of rows to move, negative numbers move backward, positive numbers move forward
+    /// Returns: A Bool indicating whether the row pointer was moved
+    func moveTo(offset:Int) -> Bool
+    
+    /// moveToFirst: Move the row pointer to the first row
+    /// Returns: A Bool indicating whether the row pointer was moved
+    func moveToFirst() -> Bool
+    
+    /// moveToLast: Move the row pointer to the last row
+    /// Returns: A Bool indicating whether the row pointer was moved
+    func moveToLast() -> Bool
+    
+    /// moveToPosition: Move the row pointer to a specified row
+    /// Parameter absolutePosition: The position of the row to move the row pointer to
+    /// Returns: A Bool indicating whether the row pointer was moved
+    func moveToPosition(absolutePosition:Int) -> Bool
+    
+    /// next: Move the row pointer to the next row
+    /// Returns: A Bool indicating whether the row pointer was moved
+    func next() -> Bool
+    
+    /// previous: Move the row pointer to the previous row
+    /// Returns: A Bool indicating whether the row pointer was moved
+    func previous() -> Bool
+    
+    /// rowCount: Answers the number of rows in the cursor or -1 if not known
+    /// Returns: The number of rows in the cursor
+    func rowCount() -> Int
+    
+    /// stringFor: Answer the String value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The String value of the requested column
+    func stringFor(columnName:String) -> String
+    
+    /// stringFor: Answer the String value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The String value of the requested column
+    func stringFor(columnIndex:Int) -> String
+    
+    /// unsignedLongLongIntFor: Answer the UInt64 value of the column at the specified name
+    /// Parameter columnName: The name of the column being referenced
+    /// Returns: The UInt64 value of the requested column
+    func unsignedLongLongIntFor(columnName:String) -> UInt64
+    
+    /// unsignedLongLongIntFor: Answer the UInt64 value of the column at the specified index
+    /// Parameter columnIndex: The index of the column being referenced
+    /// Returns: The UInt64 value of the requested column
+    func unsignedLongLongIntFor(columnIndex:Int) -> UInt64
+}
---
+++ database/SQLiteDatabase.swift
@@ -0,0 +1,96 @@
+//
+// Created by Jeffrey Roberts on 7/12/15.
+// Copyright (c) 2015 NimbleNoggin.io. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * SQLiteDatabase is a protocol that desribes the interface to a sqlite3 database. It provides an
+ * interface for opening and closing, starting, rolling back or committing transactions and
+ * executing SQL statements.
+ */
+protocol SQLiteDatabase {
+    /// changes: The number of rows inserted/updated/deleted from the last successful SQL statement
+    var changes:Int {get}
+
+    /// isOpen: A boolean indicating whether the database is currently open
+    var isOpen:Bool {get}
+
+    /// lastInsertedRowId: The unique id of the row last inserted
+    var lastInsertedRowId:Int {get}
+
+    /// path: THe absolute path to the sqlite3 database file
+    var path:String? {get}
+
+    /// init: Initializer for a SQLiteDatabase implementation
+    /// Parameter path: The absolute path to the sqlite3 database file.
+    /// - non-nil path: Identifies the actual path to the sqlite3 database file including the filename
+    /// - nil: Identifies an in-memory database
+    init(path:String?)
+
+    /// open: Open the database
+    /// Returns: A boolean indicating whether or not the database was successfully opened
+    /// Throws: A SQLiteDatabaseError if the database could not be opened
+    func open() throws -> Bool
+
+    /// close: Close the database
+    /// Returns: A boolean indicating whether or not the database was successfully closed
+    /// THrows: A SQLiteDatabaseError if the database could not be opened
+    func close() throws -> Bool
+
+    /// startTransaction: Starts a database transaction
+    /// Returns: A boolean indicating whether or not a database transaction was started
+    /// Throws: A SQLiteDatabaseError if the transaction was not started
+    func startTransaction() throws -> Bool
+
+    /// commit: Commits an existing database transaction
+    /// Returns: A boolean indicating whether or not the commit was successful
+    /// Thows: A SQLiteDatbaseError if the transaction was not committed
+    func commit() throws -> Bool
+
+    /// rollback: Rolls back an existing database transaction
+    /// Returns: A boolean indicting whether or not the transaction as successfully rolled back
+    /// Throws: A SQLiterDatabaseError if the transation was not rolled bak
+    func rollback() throws -> Bool
+
+    /// executeUpdate: Executes a SQL statement that updates the database in some way
+    /// Parameter sqlString: THe SQL string to execute
+    /// Returns: The number of rows inserted/updated/deleted
+    /// THrows: A SQLiteDatbaseError if the statement fails
+    func executeUpdate(sqlString: String) throws -> Int
+
+    /// executeUpdate: Executes a SQL statement that updates the database in some way
+    /// Parameter sqlString: THe SQL string to execute
+    /// Parameter parameters: An Array of parameters that are bound to parameter markers in the SQL
+    /// Returns: The number of rows inserted/updated/deleted
+    /// THrows: A SQLiteDatbaseError if the statement fails
+    func executeUpdate(sqlString: String, parameters:[AnyObject?]) throws -> Int
+
+    /// executeUpdate: Executes a SQL statement that updates the database in some way
+    /// Parameter sqlString: THe SQL string to execute
+    /// Parameter parameters: A Dictionary of parameters that are bound to bind variables in the SQL
+    /// Returns: The number of rows inserted/updated/deleted
+    /// THrows: A SQLiteDatbaseError if the statement fails
+    func executeUpdate(sqlString: String, parameters:[String:AnyObject?]) throws -> Int
+
+    /// executeQuery: Executes a SQL query statement that returns zero or more rows
+    /// Parameter sqlString: THe SQL string to execute
+    /// Returns: A cursor containing the query results
+    /// THrows: A SQLiteDatbaseError if the statement fails
+    func executeQuery(sqlString: String) throws -> Cursor
+
+    /// executeQuery: Executes a SQL query statement that returns zero or more rows
+    /// Parameter sqlString: THe SQL string to execute
+    /// Parameter parameters: An Array of parameters that are bound to parameter markers in the SQL
+    /// Returns: The number of rows inserted/updated/deleted
+    /// THrows: A SQLiteDatbaseError if the statement fails
+    func executeQuery(sqlString: String, parameters:[AnyObject?]) throws -> Cursor
+
+    /// executeQuery: Executes a SQL query statement that returns zero or more rows
+    /// Parameter sqlString: THe SQL string to execute
+    /// Parameter parameters: A Dictionary of parameters that are bound to bind variables in the SQL
+    /// Returns: The number of rows inserted/updated/deleted
+    /// THrows: A SQLiteDatbaseError if the statement fails
+    func executeQuery(sqlString: String, parameters:[String:AnyObject?]) throws -> Cursor
+}
